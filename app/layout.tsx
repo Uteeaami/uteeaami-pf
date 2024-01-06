45dfb7f52a656658407eb7839de584ee14c78b0b
unknown <eetu.pienimaa@gmail.com>
2024-01-06 10:43:04 +0200

    Added analytics, and refactored

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header/>
         {children}
+        <Analytics/>
         <Footer/>
       </body>
     </html>
